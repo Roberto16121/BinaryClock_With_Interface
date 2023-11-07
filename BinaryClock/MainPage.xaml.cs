1a54446f88717666c5d31022be2366ab434f9e5f
Lazar Roberto-Claudiu <lazarroberto121@gmail.com>
2023-11-07 13:03:26 +0200

    Add ConvertToBinary function and the aditional ones

--- BinaryClock/MainPage.xaml.cs
+++ BinaryClock/MainPage.xaml.cs
@@ -5,31 +5,207 @@ namespace BinaryClock
 {
     public partial class MainPage : ContentPage
     {
+        List<Border> Hour1 = new List<Border>();
+        List<Border> Hour2 = new List<Border>();
+
+        List<Border> Minute1 = new List<Border>();
+        List<Border> Minute2 = new List<Border>();
+
+        List<Border> Second1 = new List<Border>();
+        List<Border> Second2 = new List<Border>();
+
+        Color On, Off;
+
+        void Init()
+        {
+            Hour1.Add(Hour0_1);
+            Hour1.Add(Hour0_2);
+            Hour2.Add(Hour1_1);
+            Hour2.Add(Hour1_2);
+
+            Minute1.Add(Minute0_1);
+            Minute1.Add(Minute0_2);
+            Minute1.Add(Minute0_3);
+            Minute2.Add(Minute1_1);
+            Minute2.Add(Minute1_2);
+            Minute2.Add(Minute1_3);
+            Minute2.Add(Minute1_4);
+
+            Second1.Add(Seconds0_1);
+            Second1.Add(Seconds0_2);
+            Second1.Add(Seconds0_3);
+            Second2.Add(Seconds1_1);
+            Second2.Add(Seconds1_2);
+            Second2.Add(Seconds1_3);
+            Second2.Add(Seconds1_4);
+
+
+        }
 
         public MainPage()
         {
             InitializeComponent();
+            Init();
+            On = new Color(191, 64, 191);
+            Off = new Color(108, 108, 108);
+        }
+        bool IsValid = true;
+        void ConvertToBinary()
+        {
+            if (!IsValid)
+                return;
+            string text = TextEntry.Text;
+            if (text.Length < 8)
+                return;
+            string[] split = text.Split(':');  // dau split stringul dupa ':'
+            
+            int hour1 = int.Parse(split[0][0].ToString());
+            int hour2 = int.Parse(split[0][1].ToString());
+
+            string h1 = ConvertToDecimal(hour1);
+            string h2 = ConvertToDecimal(hour2);
+
+            SetHour(h1, h2);
+            Trace.WriteLine(h1 + " " + h2);
+
+            int min1 = int.Parse(split[1][0].ToString());
+            int min2 = int.Parse(split[1][1].ToString());
+
+            string m1 = ConvertToDecimal(min1);
+            string m2 = ConvertToDecimal(min2);
+
+            SetMinute(m1,m2);
+            Trace.WriteLine(m1 + " " + m2);
+
+            int sec1 = int.Parse(split[2][0].ToString());
+            int sec2 = int.Parse(split[2][1].ToString());
+
+            string s1 = ConvertToDecimal(sec1);
+            string s2 = ConvertToDecimal(sec2);
+
+            SetSeconds(s1, s2);
+            Trace.WriteLine(s1 + " " + s2);
+
         }
 
-        void OnConvertClick(object sender, EventArgs e)
+        void SetHour(string h1, string h2)
         {
-            Console.WriteLine(TextEntry.Text);
+            for(int i=0;i<h1.Length;i++)
+            {
+                if (h1[i] == '1')
+                {
+                    Hour1[i].Background = On;
+                }
+            }
+            for (int i = 0; i < h2.Length; i++)
+            {
+                if (h2[i] == '1')
+                {
+                    Hour2[i].Background = On;
+                }
+            }
+        }
+
+        void SetMinute(string m1, string m2)
+        {
+            for (int i = 0; i < m1.Length; i++)
+            {
+                if (m1[i] == '1')
+                {
+                    Minute1[i].Background = On;
+                }
+            }
+            for (int i = 0; i < m2.Length; i++)
+            {
+                if (m2[i] == '1')
+                {
+                    Minute2[i].Background = On;
+                }
+            }
+        }
+        void SetSeconds(string s1, string s2)
+        {
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == '1')
+                {
+                    Second1[i].Background = On;
+                }
+            }
+            for (int i = 0; i < s2.Length; i++)
+            {
+                if (s2[i] == '1')
+                {
+                    Second2[i].Background = On;
+                }
+            }
+        }
+
+        void ClearScreen()
+        {
+            foreach(var item in Hour1)
+                item.Background = Off;
+            foreach (var item in Hour2)
+                item.Background = Off;
+            foreach (var item in Minute1)
+                item.Background = Off;
+            foreach (var item in Minute2)
+                item.Background = Off;
+            foreach (var item in Second1)
+                item.Background = Off;
+            foreach (var item in Second2)
+                item.Background = Off;
+        }
+
+        String ConvertToDecimal(int nr)
+        {
+            string toRet = "";
+            while(nr > 0)
+            {
+                toRet += (nr%2).ToString();
+                nr /= 2;
+            }
+            return toRet;
         }
 
         void CheckIfValidInput(object sender, EventArgs e)
         {
+            ClearScreen();
             string text = TextEntry.Text;
             //string letters = new string(TextEntry.Text.Where(char.IsLetter).ToArray());
-            bool NoBueno = false;
+            IsValid = true;
             foreach (char c in text)
-                if (!char.IsDigit(c) && c != ':')
-                    NoBueno = true;
+                if (!char.IsDigit(c) && c != ':') // verific daca userul a introdus doar cifre si :
+                    IsValid = false;
+            if (text.Length >= 1) // daca ora incepe cu 3x:xx:xx sau mai mare
+            {
+                if (text[0] > '2')
+                    IsValid = false;
+            }
+            if (text.Length >= 2) // daca ora are forma x5:xx:xx sau mai mare
+            {
+                if (text[0] > '2' || text[1] > '3')
+                    IsValid = false;
+            }
+            if (text.Length >= 4)
+            {
+                if (text[3] > '5') // daca minutele incep cu x:x5:xx sau mai mare
+                    IsValid = false;
+            }
+            if(text.Length >= 7)
+            {
+                if (text[6] > '5')
+                    IsValid = false;
+            }
+            
 
-            if(NoBueno)
+            if (!IsValid)
                 TextEntry.TextColor = new Color(255, 0, 0);
             else
                 TextEntry.TextColor = new Color(0, 0, 0);
-            
+
+            if (text.Length > 7 && IsValid)
+                ConvertToBinary();
 
         }
 
